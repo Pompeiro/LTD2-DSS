352eafaea518304d10f1910152f067fed2c531fb
Pompeiro <60773657+Pompeiro@users.noreply.github.com>
2025-03-22 09:45:01 +0100

    Replace easyocr with tesseract, add read leak from event text on left side (#15)

--- backend/app/localstack/main.py
+++ backend/app/localstack/main.py
@@ -8,6 +8,7 @@ from app.localstack.navigation import (
 from app.localstack.sandbox_actions import (
     place_towers_and_wait_until_leak,
 )
+from app.localstack.views import sandbox_view
 
 
 def navigate_from_main_to_sandbox():
@@ -20,8 +21,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
+    f = sandbox_view
+    f.expect_ready_button_to_be_in_view()
     place_towers_and_wait_until_leak(1, 3)
     # navigate_from_main_to_sandbox()
+
+    # set_initial_sandbox_view_position()
 
 
 if __name__ == "__main__":
