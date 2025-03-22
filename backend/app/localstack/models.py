d44001da83fc451cfbbe37dbebd8e88586c1e16c
Pompeiro <pompeiro10@wp.pl>
2025-03-22 18:49:05 +0100

    Add rectangle model part 1, need to add tl,br for current center only elements

---
+++ backend/app/localstack/models.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+import pyautogui
+from pydantic import BaseModel, computed_field
+
+
+class Point(BaseModel):
+    x: int
+    y: int
+
+
+class Rectangle(BaseModel):
+    tl: Point
+    br: Point
+
+    @computed_field
+    def width(self) -> int:
+        return self.br.x - self.tl.x
+
+    @computed_field
+    def height(self) -> int:
+        return self.br.y - self.tl.y
+
+    @computed_field
+    def center(self) -> Point:
+        x: int = self.tl.x + (self.width // 2)
+        y: int = self.tl.y + (self.height // 2)
+        return Point(x=x, y=y)
+
+    @computed_field
+    def region(self) -> tuple[int, int, int, int]:
+        return (self.tl.x, self.tl.y, self.br.x, self.br.y)
+
+    @computed_field
+    def region_relative(self) -> tuple[int, int, int, int]:
+        return (self.tl.x, self.tl.y, self.width, self.height)
+
+
+class ActionableElement(BaseModel):
+    rectangle: Rectangle
+    image_path: Path | None = None
+    is_occupied: bool = False
+
+    def click(self, is_second_display: bool = True):
+        x, y = self.center
+        if is_second_display:
+            x = x + 1920
+        pyautogui.click(x, y)
--- backend/app/playground_area_coordinates.py
+++ backend/app/playground_area_coordinates.py
@@ -1,65 +1,44 @@
-import pyautogui
-from pydantic import BaseModel, computed_field
-
-
-class GridRectangle(BaseModel):
-    tl: tuple[int, int]
-    br: tuple[int, int]
-    is_occupied: bool = False
-
-    @computed_field
-    def center(self) -> tuple[int, int]:
-        return (
-            self.tl[0] + ((self.br[0] - self.tl[0]) // 2),
-            self.tl[1] + ((self.br[1] - self.tl[1]) // 2),
-        )
-
-    def click(self, is_second_display: bool = True) -> None:
-        x, y = self.center
-        if is_second_display:
-            x = x + 1920
-        pyautogui.click(x, y)
-
+from app.localstack.models import Point, Rectangle
 
 OFFSET = (718, 348)
 
 ROW_1_Y = 5 + OFFSET[1]
 
 ROW_1_1_X = 65 + OFFSET[0]
-rectangle_1_1 = GridRectangle(
-    tl=(ROW_1_1_X, ROW_1_Y), br=(ROW_1_1_X + 38, ROW_1_Y + 31)
+rectangle_1_1 = Rectangle(
+    tl=Point(x=ROW_1_1_X, y=ROW_1_Y), br=Point(x=ROW_1_1_X + 38, y=ROW_1_Y + 31)
 )
 ROW_1_2_X = 104 + OFFSET[0]
-rectangle_1_2 = GridRectangle(
-    tl=(ROW_1_2_X, ROW_1_Y), br=(ROW_1_2_X + 38, ROW_1_Y + 31)
+rectangle_1_2 = Rectangle(
+    tl=Point(x=ROW_1_2_X, y=ROW_1_Y), br=Point(x=ROW_1_2_X + 38, y=ROW_1_Y + 31)
 )
 ROW_1_3_X = 143 + OFFSET[0]
-rectangle_1_3 = GridRectangle(
-    tl=(ROW_1_3_X, ROW_1_Y), br=(ROW_1_3_X + 40, ROW_1_Y + 31)
+rectangle_1_3 = Rectangle(
+    tl=Point(x=ROW_1_3_X, y=ROW_1_Y), br=Point(x=ROW_1_3_X + 40, y=ROW_1_Y + 31)
 )
 ROW_1_4_X = 183 + OFFSET[0]
-rectangle_1_4 = GridRectangle(
-    tl=(ROW_1_4_X, ROW_1_Y), br=(ROW_1_4_X + 40, ROW_1_Y + 31)
+rectangle_1_4 = Rectangle(
+    tl=Point(x=ROW_1_4_X, y=ROW_1_Y), br=Point(x=ROW_1_4_X + 40, y=ROW_1_Y + 31)
 )
 ROW_1_5_X = 222 + OFFSET[0]
-rectangle_1_5 = GridRectangle(
-    tl=(ROW_1_5_X, ROW_1_Y), br=(ROW_1_5_X + 41, ROW_1_Y + 31)
+rectangle_1_5 = Rectangle(
+    tl=Point(x=ROW_1_5_X, y=ROW_1_Y), br=Point(x=ROW_1_5_X + 41, y=ROW_1_Y + 31)
 )
 ROW_1_6_X = 262 + OFFSET[0]
-rectangle_1_6 = GridRectangle(
-    tl=(ROW_1_6_X, ROW_1_Y), br=(ROW_1_6_X + 40, ROW_1_Y + 31)
+rectangle_1_6 = Rectangle(
+    tl=Point(x=ROW_1_6_X, y=ROW_1_Y), br=Point(x=ROW_1_6_X + 40, y=ROW_1_Y + 31)
 )
 ROW_1_7_X = 301 + OFFSET[0]
-rectangle_1_7 = GridRectangle(
-    tl=(ROW_1_7_X, ROW_1_Y), br=(ROW_1_7_X + 41, ROW_1_Y + 31)
+rectangle_1_7 = Rectangle(
+    tl=Point(x=ROW_1_7_X, y=ROW_1_Y), br=Point(x=ROW_1_7_X + 41, y=ROW_1_Y + 31)
 )
 ROW_1_8_X = 341 + OFFSET[0]
-rectangle_1_8 = GridRectangle(
-    tl=(ROW_1_8_X, ROW_1_Y), br=(ROW_1_8_X + 41, ROW_1_Y + 31)
+rectangle_1_8 = Rectangle(
+    tl=Point(x=ROW_1_8_X, y=ROW_1_Y), br=Point(x=ROW_1_8_X + 41, y=ROW_1_Y + 31)
 )
 ROW_1_9_X = 381 + OFFSET[0]
-rectangle_1_9 = GridRectangle(
-    tl=(ROW_1_9_X, ROW_1_Y), br=(ROW_1_9_X + 41, ROW_1_Y + 31)
+rectangle_1_9 = Rectangle(
+    tl=Point(x=ROW_1_9_X, y=ROW_1_Y), br=Point(x=ROW_1_9_X + 41, y=ROW_1_Y + 31)
 )
 
 row_1 = [
@@ -77,40 +56,40 @@ row_1 = [
 ROW_2_Y = 36 + OFFSET[1]
 
 ROW_2_1_X = 61 + OFFSET[0]
-rectangle_2_1 = GridRectangle(
-    tl=(ROW_2_1_X, ROW_2_Y), br=(ROW_2_1_X + 39, ROW_2_Y + 33)
+rectangle_2_1 = Rectangle(
+    tl=Point(x=ROW_2_1_X, y=ROW_2_Y), br=Point(x=ROW_2_1_X + 39, y=ROW_2_Y + 33)
 )
 ROW_2_2_X = 101 + OFFSET[0]
-rectangle_2_2 = GridRectangle(
-    tl=(ROW_2_2_X, ROW_2_Y), br=(ROW_2_2_X + 41, ROW_2_Y + 32)
+rectangle_2_2 = Rectangle(
+    tl=Point(x=ROW_2_2_X, y=ROW_2_Y), br=Point(x=ROW_2_2_X + 41, y=ROW_2_Y + 32)
 )
 ROW_2_3_X = 142 + OFFSET[0]
-rectangle_2_3 = GridRectangle(
-    tl=(ROW_2_3_X, ROW_2_Y), br=(ROW_2_3_X + 40, ROW_2_Y + 33)
+rectangle_2_3 = Rectangle(
+    tl=Point(x=ROW_2_3_X, y=ROW_2_Y), br=Point(x=ROW_2_3_X + 40, y=ROW_2_Y + 33)
 )
 ROW_2_4_X = 182 + OFFSET[0]
-rectangle_2_4 = GridRectangle(
-    tl=(ROW_2_4_X, ROW_2_Y), br=(ROW_2_4_X + 41, ROW_2_Y + 32)
+rectangle_2_4 = Rectangle(
+    tl=Point(x=ROW_2_4_X, y=ROW_2_Y), br=Point(x=ROW_2_4_X + 41, y=ROW_2_Y + 32)
 )
 ROW_2_5_X = 222 + OFFSET[0]
-rectangle_2_5 = GridRectangle(
-    tl=(ROW_2_5_X, ROW_2_Y), br=(ROW_2_5_X + 42, ROW_2_Y + 32)
+rectangle_2_5 = Rectangle(
+    tl=Point(x=ROW_2_5_X, y=ROW_2_Y), br=Point(x=ROW_2_5_X + 42, y=ROW_2_Y + 32)
 )
 ROW_2_6_X = 262 + OFFSET[0]
-rectangle_2_6 = GridRectangle(
-    tl=(ROW_2_6_X, ROW_2_Y), br=(ROW_2_6_X + 42, ROW_2_Y + 32)
+rectangle_2_6 = Rectangle(
+    tl=Point(x=ROW_2_6_X, y=ROW_2_Y), br=Point(x=ROW_2_6_X + 42, y=ROW_2_Y + 32)
 )
 ROW_2_7_X = 301 + OFFSET[0]
-rectangle_2_7 = GridRectangle(
-    tl=(ROW_2_7_X, ROW_2_Y), br=(ROW_2_7_X + 42, ROW_2_Y + 33)
+rectangle_2_7 = Rectangle(
+    tl=Point(x=ROW_2_7_X, y=ROW_2_Y), br=Point(x=ROW_2_7_X + 42, y=ROW_2_Y + 33)
 )
 ROW_2_8_X = 343 + OFFSET[0]
-rectangle_2_8 = GridRectangle(
-    tl=(ROW_2_8_X, ROW_2_Y), br=(ROW_2_8_X + 42, ROW_2_Y + 33)
+rectangle_2_8 = Rectangle(
+    tl=Point(x=ROW_2_8_X, y=ROW_2_Y), br=Point(x=ROW_2_8_X + 42, y=ROW_2_Y + 33)
 )
 ROW_2_9_X = 384 + OFFSET[0]
-rectangle_2_9 = GridRectangle(
-    tl=(ROW_2_9_X, ROW_2_Y), br=(ROW_2_9_X + 43, ROW_2_Y + 32)
+rectangle_2_9 = Rectangle(
+    tl=Point(x=ROW_2_9_X, y=ROW_2_Y), br=Point(x=ROW_2_9_X + 43, y=ROW_2_Y + 32)
 )
 
 row_2 = [
@@ -128,40 +107,40 @@ row_2 = [
 ROW_3_Y = 68 + OFFSET[1]
 
 ROW_3_1_X = 57 + OFFSET[0]
-rectangle_3_1 = GridRectangle(
-    tl=(ROW_3_1_X, ROW_3_Y), br=(ROW_3_1_X + 41, ROW_3_Y + 34)
+rectangle_3_1 = Rectangle(
+    tl=Point(x=ROW_3_1_X, y=ROW_3_Y), br=Point(x=ROW_3_1_X + 41, y=ROW_3_Y + 34)
 )
 ROW_3_2_X = 98 + OFFSET[0]
-rectangle_3_2 = GridRectangle(
-    tl=(ROW_3_2_X, ROW_3_Y), br=(ROW_3_2_X + 41, ROW_3_Y + 35)
+rectangle_3_2 = Rectangle(
+    tl=Point(x=ROW_3_2_X, y=ROW_3_Y), br=Point(x=ROW_3_2_X + 41, y=ROW_3_Y + 35)
 )
 ROW_3_3_X = 139 + OFFSET[0]
-rectangle_3_3 = GridRectangle(
-    tl=(ROW_3_3_X, ROW_3_Y), br=(ROW_3_3_X + 42, ROW_3_Y + 35)
+rectangle_3_3 = Rectangle(
+    tl=Point(x=ROW_3_3_X, y=ROW_3_Y), br=Point(x=ROW_3_3_X + 42, y=ROW_3_Y + 35)
 )
 ROW_3_4_X = 180 + OFFSET[0]
-rectangle_3_4 = GridRectangle(
-    tl=(ROW_3_4_X, ROW_3_Y), br=(ROW_3_4_X + 42, ROW_3_Y + 35)
+rectangle_3_4 = Rectangle(
+    tl=Point(x=ROW_3_4_X, y=ROW_3_Y), br=Point(x=ROW_3_4_X + 42, y=ROW_3_Y + 35)
 )
 ROW_3_5_X = 222 + OFFSET[0]
-rectangle_3_5 = GridRectangle(
-    tl=(ROW_3_5_X, ROW_3_Y), br=(ROW_3_5_X + 42, ROW_3_Y + 35)
+rectangle_3_5 = Rectangle(
+    tl=Point(x=ROW_3_5_X, y=ROW_3_Y), br=Point(x=ROW_3_5_X + 42, y=ROW_3_Y + 35)
 )
 ROW_3_6_X = 263 + OFFSET[0]
-rectangle_3_6 = GridRectangle(
-    tl=(ROW_3_6_X, ROW_3_Y), br=(ROW_3_6_X + 43, ROW_3_Y + 35)
+rectangle_3_6 = Rectangle(
+    tl=Point(x=ROW_3_6_X, y=ROW_3_Y), br=Point(x=ROW_3_6_X + 43, y=ROW_3_Y + 35)
 )
 ROW_3_7_X = 304 + OFFSET[0]
-rectangle_3_7 = GridRectangle(
-    tl=(ROW_3_7_X, ROW_3_Y), br=(ROW_3_7_X + 43, ROW_3_Y + 35)
+rectangle_3_7 = Rectangle(
+    tl=Point(x=ROW_3_7_X, y=ROW_3_Y), br=Point(x=ROW_3_7_X + 43, y=ROW_3_Y + 35)
 )
 ROW_3_8_X = 346 + OFFSET[0]
-rectangle_3_8 = GridRectangle(
-    tl=(ROW_3_8_X, ROW_3_Y), br=(ROW_3_8_X + 42, ROW_3_Y + 34)
+rectangle_3_8 = Rectangle(
+    tl=Point(x=ROW_3_8_X, y=ROW_3_Y), br=Point(x=ROW_3_8_X + 42, y=ROW_3_Y + 34)
 )
 ROW_3_9_X = 387 + OFFSET[0]
-rectangle_3_9 = GridRectangle(
-    tl=(ROW_3_9_X, ROW_3_Y), br=(ROW_3_9_X + 42, ROW_3_Y + 35)
+rectangle_3_9 = Rectangle(
+    tl=Point(x=ROW_3_9_X, y=ROW_3_Y), br=Point(x=ROW_3_9_X + 42, y=ROW_3_Y + 35)
 )
 
 row_3 = [
@@ -180,40 +159,40 @@ row_3 = [
 ROW_4_Y = 102 + OFFSET[1]
 
 ROW_4_1_X = 53 + OFFSET[0]
-rectangle_4_1 = GridRectangle(
-    tl=(ROW_4_1_X, ROW_4_Y), br=(ROW_4_1_X + 42, ROW_4_Y + 35)
+rectangle_4_1 = Rectangle(
+    tl=Point(x=ROW_4_1_X, y=ROW_4_Y), br=Point(x=ROW_4_1_X + 42, y=ROW_4_Y + 35)
 )
 ROW_4_2_X = 95 + OFFSET[0]
-rectangle_4_2 = GridRectangle(
-    tl=(ROW_4_2_X, ROW_4_Y), br=(ROW_4_2_X + 42, ROW_4_Y + 36)
+rectangle_4_2 = Rectangle(
+    tl=Point(x=ROW_4_2_X, y=ROW_4_Y), br=Point(x=ROW_4_2_X + 42, y=ROW_4_Y + 36)
 )
 ROW_4_3_X = 137 + OFFSET[0]
-rectangle_4_3 = GridRectangle(
-    tl=(ROW_4_3_X, ROW_4_Y), br=(ROW_4_3_X + 43, ROW_4_Y + 36)
+rectangle_4_3 = Rectangle(
+    tl=Point(x=ROW_4_3_X, y=ROW_4_Y), br=Point(x=ROW_4_3_X + 43, y=ROW_4_Y + 36)
 )
 ROW_4_4_X = 179 + OFFSET[0]
-rectangle_4_4 = GridRectangle(
-    tl=(ROW_4_4_X, ROW_4_Y), br=(ROW_4_4_X + 44, ROW_4_Y + 35)
+rectangle_4_4 = Rectangle(
+    tl=Point(x=ROW_4_4_X, y=ROW_4_Y), br=Point(x=ROW_4_4_X + 44, y=ROW_4_Y + 35)
 )
 ROW_4_5_X = 221 + OFFSET[0]
-rectangle_4_5 = GridRectangle(
-    tl=(ROW_4_5_X, ROW_4_Y), br=(ROW_4_5_X + 44, ROW_4_Y + 36)
+rectangle_4_5 = Rectangle(
+    tl=Point(x=ROW_4_5_X, y=ROW_4_Y), br=Point(x=ROW_4_5_X + 44, y=ROW_4_Y + 36)
 )
 ROW_4_6_X = 262 + OFFSET[0]
-rectangle_4_6 = GridRectangle(
-    tl=(ROW_4_6_X, ROW_4_Y), br=(ROW_4_6_X + 45, ROW_4_Y + 36)
+rectangle_4_6 = Rectangle(
+    tl=Point(x=ROW_4_6_X, y=ROW_4_Y), br=Point(x=ROW_4_6_X + 45, y=ROW_4_Y + 36)
 )
 ROW_4_7_X = 306 + OFFSET[0]
-rectangle_4_7 = GridRectangle(
-    tl=(ROW_4_7_X, ROW_4_Y), br=(ROW_4_7_X + 43, ROW_4_Y + 35)
+rectangle_4_7 = Rectangle(
+    tl=Point(x=ROW_4_7_X, y=ROW_4_Y), br=Point(x=ROW_4_7_X + 43, y=ROW_4_Y + 35)
 )
 ROW_4_8_X = 347 + OFFSET[0]
-rectangle_4_8 = GridRectangle(
-    tl=(ROW_4_8_X, ROW_4_Y), br=(ROW_4_8_X + 44, ROW_4_Y + 36)
+rectangle_4_8 = Rectangle(
+    tl=Point(x=ROW_4_8_X, y=ROW_4_Y), br=Point(x=ROW_4_8_X + 44, y=ROW_4_Y + 36)
 )
 ROW_4_9_X = 390 + OFFSET[0]
-rectangle_4_9 = GridRectangle(
-    tl=(ROW_4_9_X, ROW_4_Y), br=(ROW_4_9_X + 43, ROW_4_Y + 35)
+rectangle_4_9 = Rectangle(
+    tl=Point(x=ROW_4_9_X, y=ROW_4_Y), br=Point(x=ROW_4_9_X + 43, y=ROW_4_Y + 35)
 )
 
 row_4 = [
@@ -231,40 +210,40 @@ row_4 = [
 ROW_5_Y = 136 + OFFSET[1]
 
 ROW_5_1_X = 48 + OFFSET[0]
-rectangle_5_1 = GridRectangle(
-    tl=(ROW_5_1_X, ROW_5_Y), br=(ROW_5_1_X + 43, ROW_5_Y + 38)
+rectangle_5_1 = Rectangle(
+    tl=Point(x=ROW_5_1_X, y=ROW_5_Y), br=Point(x=ROW_5_1_X + 43, y=ROW_5_Y + 38)
 )
 ROW_5_2_X = 91 + OFFSET[0]
-rectangle_5_2 = GridRectangle(
-    tl=(ROW_5_2_X, ROW_5_Y), br=(ROW_5_2_X + 44, ROW_5_Y + 38)
+rectangle_5_2 = Rectangle(
+    tl=Point(x=ROW_5_2_X, y=ROW_5_Y), br=Point(x=ROW_5_2_X + 44, y=ROW_5_Y + 38)
 )
 ROW_5_3_X = 135 + OFFSET[0]
-rectangle_5_3 = GridRectangle(
-    tl=(ROW_5_3_X, ROW_5_Y), br=(ROW_5_3_X + 43, ROW_5_Y + 38)
+rectangle_5_3 = Rectangle(
+    tl=Point(x=ROW_5_3_X, y=ROW_5_Y), br=Point(x=ROW_5_3_X + 43, y=ROW_5_Y + 38)
 )
 ROW_5_4_X = 178 + OFFSET[0]
-rectangle_5_4 = GridRectangle(
-    tl=(ROW_5_4_X, ROW_5_Y), br=(ROW_5_4_X + 44, ROW_5_Y + 39)
+rectangle_5_4 = Rectangle(
+    tl=Point(x=ROW_5_4_X, y=ROW_5_Y), br=Point(x=ROW_5_4_X + 44, y=ROW_5_Y + 39)
 )
 ROW_5_5_X = 221 + OFFSET[0]
-rectangle_5_5 = GridRectangle(
-    tl=(ROW_5_5_X, ROW_5_Y), br=(ROW_5_5_X + 44, ROW_5_Y + 39)
+rectangle_5_5 = Rectangle(
+    tl=Point(x=ROW_5_5_X, y=ROW_5_Y), br=Point(x=ROW_5_5_X + 44, y=ROW_5_Y + 39)
 )
 ROW_5_6_X = 264 + OFFSET[0]
-rectangle_5_6 = GridRectangle(
-    tl=(ROW_5_6_X, ROW_5_Y), br=(ROW_5_6_X + 44, ROW_5_Y + 40)
+rectangle_5_6 = Rectangle(
+    tl=Point(x=ROW_5_6_X, y=ROW_5_Y), br=Point(x=ROW_5_6_X + 44, y=ROW_5_Y + 40)
 )
 ROW_5_7_X = 306 + OFFSET[0]
-rectangle_5_7 = GridRectangle(
-    tl=(ROW_5_7_X, ROW_5_Y), br=(ROW_5_7_X + 45, ROW_5_Y + 39)
+rectangle_5_7 = Rectangle(
+    tl=Point(x=ROW_5_7_X, y=ROW_5_Y), br=Point(x=ROW_5_7_X + 45, y=ROW_5_Y + 39)
 )
 ROW_5_8_X = 349 + OFFSET[0]
-rectangle_5_8 = GridRectangle(
-    tl=(ROW_5_8_X, ROW_5_Y), br=(ROW_5_8_X + 46, ROW_5_Y + 38)
+rectangle_5_8 = Rectangle(
+    tl=Point(x=ROW_5_8_X, y=ROW_5_Y), br=Point(x=ROW_5_8_X + 46, y=ROW_5_Y + 38)
 )
 ROW_5_9_X = 393 + OFFSET[0]
-rectangle_5_9 = GridRectangle(
-    tl=(ROW_5_9_X, ROW_5_Y), br=(ROW_5_9_X + 45, ROW_5_Y + 37)
+rectangle_5_9 = Rectangle(
+    tl=Point(x=ROW_5_9_X, y=ROW_5_Y), br=Point(x=ROW_5_9_X + 45, y=ROW_5_Y + 37)
 )
 
 row_5 = [
@@ -282,40 +261,40 @@ row_5 = [
 ROW_6_Y = 174 + OFFSET[1]
 
 ROW_6_1_X = 45 + OFFSET[0]
-rectangle_6_1 = GridRectangle(
-    tl=(ROW_6_1_X, ROW_6_Y), br=(ROW_6_1_X + 44, ROW_6_Y + 39)
+rectangle_6_1 = Rectangle(
+    tl=Point(x=ROW_6_1_X, y=ROW_6_Y), br=Point(x=ROW_6_1_X + 44, y=ROW_6_Y + 39)
 )
 ROW_6_2_X = 89 + OFFSET[0]
-rectangle_6_2 = GridRectangle(
-    tl=(ROW_6_2_X, ROW_6_Y), br=(ROW_6_2_X + 44, ROW_6_Y + 39)
+rectangle_6_2 = Rectangle(
+    tl=Point(x=ROW_6_2_X, y=ROW_6_Y), br=Point(x=ROW_6_2_X + 44, y=ROW_6_Y + 39)
 )
 ROW_6_3_X = 132 + OFFSET[0]
-rectangle_6_3 = GridRectangle(
-    tl=(ROW_6_3_X, ROW_6_Y), br=(ROW_6_3_X + 45, ROW_6_Y + 38)
+rectangle_6_3 = Rectangle(
+    tl=Point(x=ROW_6_3_X, y=ROW_6_Y), br=Point(x=ROW_6_3_X + 45, y=ROW_6_Y + 38)
 )
 ROW_6_4_X = 176 + OFFSET[0]
-rectangle_6_4 = GridRectangle(
-    tl=(ROW_6_4_X, ROW_6_Y), br=(ROW_6_4_X + 45, ROW_6_Y + 39)
+rectangle_6_4 = Rectangle(
+    tl=Point(x=ROW_6_4_X, y=ROW_6_Y), br=Point(x=ROW_6_4_X + 45, y=ROW_6_Y + 39)
 )
 ROW_6_5_X = 220 + OFFSET[0]
-rectangle_6_5 = GridRectangle(
-    tl=(ROW_6_5_X, ROW_6_Y), br=(ROW_6_5_X + 45, ROW_6_Y + 39)
+rectangle_6_5 = Rectangle(
+    tl=Point(x=ROW_6_5_X, y=ROW_6_Y), br=Point(x=ROW_6_5_X + 45, y=ROW_6_Y + 39)
 )
 ROW_6_6_X = 265 + OFFSET[0]
-rectangle_6_6 = GridRectangle(
-    tl=(ROW_6_6_X, ROW_6_Y), br=(ROW_6_6_X + 45, ROW_6_Y + 40)
+rectangle_6_6 = Rectangle(
+    tl=Point(x=ROW_6_6_X, y=ROW_6_Y), br=Point(x=ROW_6_6_X + 45, y=ROW_6_Y + 40)
 )
 ROW_6_7_X = 308 + OFFSET[0]
-rectangle_6_7 = GridRectangle(
-    tl=(ROW_6_7_X, ROW_6_Y), br=(ROW_6_7_X + 46, ROW_6_Y + 39)
+rectangle_6_7 = Rectangle(
+    tl=Point(x=ROW_6_7_X, y=ROW_6_Y), br=Point(x=ROW_6_7_X + 46, y=ROW_6_Y + 39)
 )
 ROW_6_8_X = 352 + OFFSET[0]
-rectangle_6_8 = GridRectangle(
-    tl=(ROW_6_8_X, ROW_6_Y), br=(ROW_6_8_X + 46, ROW_6_Y + 39)
+rectangle_6_8 = Rectangle(
+    tl=Point(x=ROW_6_8_X, y=ROW_6_Y), br=Point(x=ROW_6_8_X + 46, y=ROW_6_Y + 39)
 )
 ROW_6_9_X = 396 + OFFSET[0]
-rectangle_6_9 = GridRectangle(
-    tl=(ROW_6_9_X, ROW_6_Y), br=(ROW_6_9_X + 45, ROW_6_Y + 39)
+rectangle_6_9 = Rectangle(
+    tl=Point(x=ROW_6_9_X, y=ROW_6_Y), br=Point(x=ROW_6_9_X + 45, y=ROW_6_Y + 39)
 )
 
 row_6 = [
@@ -334,40 +313,40 @@ row_6 = [
 ROW_7_Y = 212 + OFFSET[1]
 
 ROW_7_1_X = 40 + OFFSET[0]
-rectangle_7_1 = GridRectangle(
-    tl=(ROW_7_1_X, ROW_7_Y), br=(ROW_7_1_X + 45, ROW_7_Y + 41)
+rectangle_7_1 = Rectangle(
+    tl=Point(x=ROW_7_1_X, y=ROW_7_Y), br=Point(x=ROW_7_1_X + 45, y=ROW_7_Y + 41)
 )
 ROW_7_2_X = 85 + OFFSET[0]
-rectangle_7_2 = GridRectangle(
-    tl=(ROW_7_2_X, ROW_7_Y), br=(ROW_7_2_X + 46, ROW_7_Y + 41)
+rectangle_7_2 = Rectangle(
+    tl=Point(x=ROW_7_2_X, y=ROW_7_Y), br=Point(x=ROW_7_2_X + 46, y=ROW_7_Y + 41)
 )
 ROW_7_3_X = 130 + OFFSET[0]
-rectangle_7_3 = GridRectangle(
-    tl=(ROW_7_3_X, ROW_7_Y), br=(ROW_7_3_X + 45, ROW_7_Y + 42)
+rectangle_7_3 = Rectangle(
+    tl=Point(x=ROW_7_3_X, y=ROW_7_Y), br=Point(x=ROW_7_3_X + 45, y=ROW_7_Y + 42)
 )
 ROW_7_4_X = 175 + OFFSET[0]
-rectangle_7_4 = GridRectangle(
-    tl=(ROW_7_4_X, ROW_7_Y), br=(ROW_7_4_X + 45, ROW_7_Y + 41)
+rectangle_7_4 = Rectangle(
+    tl=Point(x=ROW_7_4_X, y=ROW_7_Y), br=Point(x=ROW_7_4_X + 45, y=ROW_7_Y + 41)
 )
 ROW_7_5_X = 220 + OFFSET[0]
-rectangle_7_5 = GridRectangle(
-    tl=(ROW_7_5_X, ROW_7_Y), br=(ROW_7_5_X + 46, ROW_7_Y + 41)
+rectangle_7_5 = Rectangle(
+    tl=Point(x=ROW_7_5_X, y=ROW_7_Y), br=Point(x=ROW_7_5_X + 46, y=ROW_7_Y + 41)
 )
 ROW_7_6_X = 266 + OFFSET[0]
-rectangle_7_6 = GridRectangle(
-    tl=(ROW_7_6_X, ROW_7_Y), br=(ROW_7_6_X + 45, ROW_7_Y + 40)
+rectangle_7_6 = Rectangle(
+    tl=Point(x=ROW_7_6_X, y=ROW_7_Y), br=Point(x=ROW_7_6_X + 45, y=ROW_7_Y + 40)
 )
 ROW_7_7_X = 311 + OFFSET[0]
-rectangle_7_7 = GridRectangle(
-    tl=(ROW_7_7_X, ROW_7_Y), br=(ROW_7_7_X + 45, ROW_7_Y + 41)
+rectangle_7_7 = Rectangle(
+    tl=Point(x=ROW_7_7_X, y=ROW_7_Y), br=Point(x=ROW_7_7_X + 45, y=ROW_7_Y + 41)
 )
 ROW_7_8_X = 356 + OFFSET[0]
-rectangle_7_8 = GridRectangle(
-    tl=(ROW_7_8_X, ROW_7_Y), br=(ROW_7_8_X + 46, ROW_7_Y + 41)
+rectangle_7_8 = Rectangle(
+    tl=Point(x=ROW_7_8_X, y=ROW_7_Y), br=Point(x=ROW_7_8_X + 46, y=ROW_7_Y + 41)
 )
 ROW_7_9_X = 400 + OFFSET[0]
-rectangle_7_9 = GridRectangle(
-    tl=(ROW_7_9_X, ROW_7_Y), br=(ROW_7_9_X + 46, ROW_7_Y + 41)
+rectangle_7_9 = Rectangle(
+    tl=Point(x=ROW_7_9_X, y=ROW_7_Y), br=Point(x=ROW_7_9_X + 46, y=ROW_7_Y + 41)
 )
 
 row_7 = [
